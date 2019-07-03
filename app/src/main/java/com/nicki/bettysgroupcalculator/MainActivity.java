271bcdb2f875add23bebadb4bf5b217fb75ae606
AC2 <donoloo4545@gmail.com>
2019-07-03 17:16:05 +0300

    the fouth update

---
+++ app/src/main/java/com/nicki/bettysgroupcalculator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.nicki.bettysgroupcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView myTextView;
+    private EditText myEditText;
+    private Button buttonone;
+    private  Button buttontwo;
+    private Button buttonthree;
+    private  Button buttonfour;
+    private Button buttonfive;
+    private  Button buttonsix;
+    private Button buttonseven;
+    private  Button buttoneight;
+    private Button buttonnine;
+    private  Button buttonten;
+    private Button buttoneleven;
+    private  Button buttonttwelve;
+    private Button button13;
+    private  Button button14;
+    private Button button15;
+    private  Button button16;
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
+
